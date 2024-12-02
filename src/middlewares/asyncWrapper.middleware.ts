6366ce97078438db6940f5d667eb10a8a879500a
Eyad-AbdElMohsen <158308690+Eyad-AbdElMohsen@users.noreply.github.com>
2024-12-02 23:46:34 +0200

    Update asyncWrapper.middleware.ts

--- src/middlewares/asyncWrapper.middleware.ts
+++ src/middlewares/asyncWrapper.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 type asyncFn = (req: Request, res: Response, next: NextFunction) => Promise<any> 
 
-const asyncWrapper = async (asyncFn: asyncFn) => {
+const asyncWrapper = (asyncFn: asyncFn) => {
     return ( req: Request, res: Response, next: NextFunction) => {
         asyncFn(req, res, next).catch((err) => {
             next(err)
