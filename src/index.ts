293450512a763b43b4b00fb15280c9ca1ea263c0
Eyad-AbdElMohsen <ahmedeyad84@gmail.com>
2024-12-06 22:48:06 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import express , {Express , Request , Response}from "express" 
-import userRouter from "./routes/user.route";
 import errorMiddleware from "./middlewares/error.middleware";
 import notFoundMiddleware from "./middlewares/notFound.middleware";
 
@@ -12,12 +11,7 @@ const port = process.env.port || 8000
 const app : Express = express();
 
 app.use(express.json())
-
-app.get("/",(req : Request ,res : Response) =>{
-    res.send("Hello from ts express");
-})
-
-app.use(userRouter)
+app.use(requestLogger)
 
 
 // glopal middleware
